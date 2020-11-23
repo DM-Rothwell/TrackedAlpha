309955278fd52c045230429093af273df7d622e6
DM-Rothwell <71714425+DM-Rothwell@users.noreply.github.com>
2020-11-23 17:54:25 +0000

    Added Wall
    Added wall to keep player inside game area.

---
+++ Assets/Scripts/BallMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove : MonoBehaviour
+{
+    //Declare Variables
+    public float speed = 35.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move ball
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     //Private Variables 
     private float speed = 10.0f;
-    private float turnSpeed = 50.0f;
+    private float turnSpeed = 100.0f;
     private float horizontalInput;
     private float fowardInput;
     //private Animation playerAnim;
-
+    public GameObject projectilePrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -26,10 +26,16 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         fowardInput = Input.GetAxis("Vertical");
 
-        //Move vechicle foward
+        //Move person foward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * fowardInput);
 
-        //Turn vehicle
+        //Turn Person
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        //'K' for projecting a ball
+        if (Input.GetKeyDown(KeyCode.K)){
+            //Shoot the projectile
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
