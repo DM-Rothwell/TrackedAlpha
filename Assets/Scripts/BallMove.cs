6130f2f55733f79240d0058b4e754b6dd78333f6
DM-Rothwell <71714425+DM-Rothwell@users.noreply.github.com>
2020-11-27 19:22:06 +0000

    Added Scripts and Levelled Out Terrain

--- Assets/Scripts/BallMove.cs
+++ Assets/Scripts/BallMove.cs
@@ -16,6 +16,9 @@ public class BallMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Generate ball at spawn pos
+        Vector3 spawnPos = new Vector3(Random.Range(0, 0), 1, 0);
+
         //Move ball
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //Private Variables 
-    private float speed = 10.0f;
+    private float speed = 15.0f;
     private float turnSpeed = 100.0f;
     private float horizontalInput;
     private float fowardInput;
