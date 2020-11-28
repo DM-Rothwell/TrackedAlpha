0fed86a98b88c6cd15333b5debfcfa553ff85389
DM-Rothwell <71714425+DM-Rothwell@users.noreply.github.com>
2020-11-28 22:29:31 +0000

    Added GUI
    GUI Includes
    - Link to github Button (currently not working)
    - Play Game Button
    - Instructions Button
    - Score Counter
    - Title

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+    private Button playButton;
+    private GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playButton = GetComponent<Button>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        
+        playButton.onClick.AddListener(PlayGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Create method for button to disappear when clicked
+    void PlayGame()
+    {
+        Debug.Log(playButton.gameObject.name + " was clicked");
+
+    }
+
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class GameManager : MonoBehaviour
+{
+    //Declare Variables
+    private int score;
+    public TextMeshProUGUI scoreText;
+    public bool gameActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //UpdateScore(0);
+
+        /*IEnumerator SpawnManager()
+        {
+            while (true)
+            {
+                UpdateScore(1);
+            }//End of while
+        }//end of IEnumerator*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void UpdateScore(int scoreAdd)
+    {
+        score += scoreAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    //Method to start game when "Play Game" is clicked
+    public void StartGame()
+    {
+        gameActive = true;
+        score = 0;
+
+        //StartCoroutine(SpawnManager());
+        UpdateScore(0);
+    }
+}
+
